e91658aad736e1fcc1fbc85007dd711528d41919
David Zemon <david@zemon.name>
2018-05-11 17:39:43 -0500

    Switch to zip install, force data dir mount

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,13 @@ ENV DEBIAN_FRONTEND=noninteractive
 RUN apt-get update
 RUN apt-get install --yes \
   wget \
-  openjdk-8-jre
+  openjdk-8-jre \
+  unzip
 
-RUN wget --quiet -O /tmp/jfrog-artifactory-cpp-ce-5.11.0.deb https://bintray.com/jfrog/artifactory-debs/download_file?file_path=pool%2Fmain%2Fj%2Fjfrog-artifactory-cpp-ce-deb%2Fjfrog-artifactory-cpp-ce-5.11.0.deb
-RUN dpkg -i /tmp/jfrog-artifactory-cpp-ce-5.11.0.deb; exit 0
-RUN apt-get install --yes --fix-broken
-RUN mv /etc/opt/jfrog/artifactory /etc/opt/jfrog/artifactory.default
+ENV ARTIFACTORY_HOME=/opt/artifactory-cpp-ce-5.11.0
+RUN wget -O /tmp/jfrog-artifactory-cpp-ce-5.11.0.zip https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-cpp-ce-5.11.0.zip
+RUN unzip /tmp/jfrog-artifactory-cpp-ce-5.11.0.zip -d /opt
+RUN mv "${ARTIFACTORY_HOME}/etc" "${ARTIFACTORY_HOME}/etc.defaults"
 
 COPY start.sh /start.sh
 RUN chmod +x /start.sh
--- start.sh
+++ start.sh
@@ -2,17 +2,22 @@
 
 
 
-if [[ ! -d /etc/opt/jfrog/artifactory ]] ; then
-    echo "Artifactory configuration files must be mounted to /etc/opt/jfrog/artifactory. Rerun with Docker's --volume argument."
+if [[ ! -d "${ARTIFACTORY_HOME}/etc" ]] ; then
+    echo "Artifactory configuration files must be mounted to '${ARTIFACTORY_HOME}/etc' - rerun with Docker's --volume argument."
     exit 1
-elif [ ! "$(ls -A /etc/opt/jfrog/artifactory)" ] ; then
+elif [ ! "$(ls -A ${ARTIFACTORY_HOME})" ] ; then
     set -e
     echo "First startup detected. Copying default configuration files."
-    find /etc/opt/jfrog/artifactory.default -type f -exec cp {} /etc/opt/jfrog/artifactory \;
+    find "${ARTIFACTORY_HOME}/etc.defaults" -type f -exec cp {} "${ARTIFACTORY_HOME}/etc" \;
 fi
 
-service artifactory start
-tail -F /var/opt/jfrog/artifactory/logs/artifactory.log &
+if [[ ! -d "${ARTIFACTORY_HOME}/data" ]] ; then
+    echo "Artifactory data directory must be mounted to '${ARTIFACTORY_HOME}/data' - rerun with Docker's --volume argument."
+    exit 2
+fi
+
+"${ARTIFACTORY_HOME}/bin/artifactory.sh" start
+tail -F "${ARTIFACTORY_HOME}/logs/artifactory.log" &
 
 # Hold here until Artifactory exits
 while pgrep -f java > /dev/null; do sleep 1; done;
